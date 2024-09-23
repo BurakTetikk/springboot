e5b00e9efa391dd4cfe733d7690f69b7aca01d34
BurakTetikk <burktetk@gmail.com>
2024-09-23 17:48:46 +0300

    entity class

---
+++ src/main/java/com/tpe/entity/Student.java
@@ -0,0 +1,66 @@
+package com.tpe.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+//@RequiredArgsConstructor // final işaretli fieldlar için constructor oluşturur
+@NoArgsConstructor
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private Long id;
+
+    @NotNull(message = "Firstname can not be null!!")
+    @NotBlank(message = "Firstname can not be whitespace") //space karakterinin eklenmemesi için kullanılır
+    @Size(  min = 3,
+            max = 25,
+            message = "Firstname '${validatedValue}' must be between {min} and {max}")
+    @Column(nullable = false, length = 25) //Hibernate kontrolü
+    //private final String name;
+    private String name;
+
+    @Column(nullable = false, length = 25)
+    //private final String lastname;
+    private String lastname;
+
+    //private final Integer grade;
+    private Integer grade;
+
+    @Column(nullable = false, length = 50, unique = true)
+    @Email(message = "Provide valid email!!")  // xxx@yy.com
+    //private final String email;
+    private String email;
+
+    //private final String phoneNumber;
+    private String phoneNumber;
+
+    @Setter(AccessLevel.NONE) // setter metodu yok
+    private LocalDateTime createDate = LocalDateTime.now();
+
+
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", grade=" + grade +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", createDate=" + createDate +
+                '}';
+    }
+}
